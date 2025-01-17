021a5f3ab5b9beed22a595cc7c9ae8606b54edd0
NataliiaYare <natusiazz599@gmail.com>
2025-01-17 14:42:32 +0000

    17.01.25

--- scripts.js
+++ scripts.js
@@ -7,3 +7,17 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+// Script for Special Offer Photo Gallery
+
+const imgContent = document.querySelectorAll(".img-content-hover");
+
+function showImgContent(e) {
+  for (var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+}
+
+document.addEventListener("mousemove", showImgContent);
